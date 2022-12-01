f14605e03a2ebc60dc5a42d7dcc152ddfec0a70f
VicS45 <118304860+VicS45@users.noreply.github.com>
2022-11-30 21:24:46 -0500

    Delete target/debug directory

--- target/debug/deps/midterm_extracredit.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Vicky\Documents\GitHub\rust_fixme\target\debug\deps\midterm_extracredit.exe: src\main.rs
-
-C:\Users\Vicky\Documents\GitHub\rust_fixme\target\debug\deps\midterm_extracredit.d: src\main.rs
-
-src\main.rs:
--- target/debug/midterm_extracredit.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Vicky\Documents\GitHub\rust_fixme\target\debug\midterm_extracredit.exe: C:\Users\Vicky\Documents\GitHub\rust_fixme\src\main.rs
