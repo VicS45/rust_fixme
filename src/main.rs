4fd30df85c61f8a3de20f2c141bc6f1526dfebc2
VicS45 <118304860+VicS45@users.noreply.github.com>
2022-11-30 21:00:44 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -23,27 +23,59 @@ impl Display for City {
 }
 
 #[derive(Debug)]
-struct Color {
+struct Color
+{
     red: u8,
     green: u8,
     blue: u8,
 }
 
-fn main() {
-    for city in [
-        City { name: "Glassboro", lat: 39.702892, lon: -75.111839 },
-        City { name: "Mullica Hill", lat: 39.73928, lon: -75.224072 },
-        City { name: "Swedesboro", lat: 39.747616, lon: -75.310463 },
-    ].iter() {
-        println!("{}", *city);
-    }
-
-    for color in [
-        Color { red: 128, green: 255, blue: 90 },
-        Color { red: 0, green: 3, blue: 254 },
-        Color { red: 0, green: 0, blue: 0 },
-    ].iter() {
-        // Hint : Fix the code so you can print it using {}
-        println!("{:?}", *color);
+impl fmt::Display for Color
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+    {
+        write!(f, "red: {}, green: {}, blue: {}", self.red, self.green, self.blue)
     }
 }
+
+
+fn main()
+{
+        for city in
+        [
+            City { name: "Glassboro", lat: 39.702892, lon: -75.111839 },
+            City { name: "Mullica Hill", lat: 39.73928, lon: -75.224072 },
+            City { name: "Swedesboro", lat: 39.747616, lon: -75.310463 },
+        ]
+        .iter()
+
+        {
+            println!("{}", *city);
+        }
+
+
+        for color in
+        [
+            Color { red: 128, green: 255, blue: 90 },
+            Color { red: 0, green: 3, blue: 254 },
+            Color { red: 0, green: 0, blue: 0 },
+        ]
+
+        .iter()
+
+
+        {
+            // Hint : Fix the code so you can print it using {}
+            println!("{}", color);
+        }
+}
+
+// "color" was originally being printed with {:?} which means that the debug implementation was being used
+// If the brackets are left empty, the corresponding argument uses the display trait instead
+
+// std::fmt has a formatting syntax composed of two parts: position or name & formatting
+// when there is an absence of both parts, the default is to pick the matching index of {} and to use the display trait
+
+// the parameter's type isn't relevant, but the traits they implement are
+// ex: vectors implement debug but not display, which is why {?} has to be used with vectors and not {}
+// source: https://stackoverflow.com/questions/40100077/what-is-the-difference-between-printlns-format-styles
